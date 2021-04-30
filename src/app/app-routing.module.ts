97f78a1ef503f9a3b17b2d3d37e5126c3833318f
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-30 20:04:05 +0300

    add the assets and general styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'Home', component: LandingComponent},
+  {path:'About', component: AboutComponent},
+  {path:'Services', component: ServicesComponent},
+  {path:'Projects', component: ProjectsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    NavbarComponent,
+    LandingComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
